9f49efda0b8454847b5643891558d080d5e0de4e
Rodrigo Marchi <rodrigo.marchi@ekinops.com>
2020-01-02 11:13:17 -0300

    added plain vuejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,10 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import Vue from "vue";
+import Message from "./components/Message.vue";
+
+new Vue({
+    render: h => h(Message)
+}).$mount("#hello")
